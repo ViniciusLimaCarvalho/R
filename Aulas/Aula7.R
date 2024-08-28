13a7bb3c561becfba70b799d9f51b4020834edba
ViniciusLimaCarvalho <viniciuscarvalho@ufu.br>
2024-08-28 18:06:34 -0300

    final

--- Aulas/Aula7.R
+++ Aulas/Aula7.R
@@ -6,7 +6,7 @@ data(penguins)
 pinguins <- penguins
 str(pinguins)
 pinguins <- pinguins[,-c(2,7,8)]
-n <- na.omit(pinguins)
+pinguins <- na.omit(pinguins)
 
 n <- round(0.8*nrow(pinguins))
 indices_treino <- sample(1:nrow(pinguins), size = n, replace = FALSE)
@@ -20,4 +20,15 @@ classe_treino <- treino$species
 classe_teste <- teste$species
 
 modelo1 <- knn(train = treino_padronizado, test = teste_padronizado, cl = classe_treino, k=1)
+mean(modelo1 == teste$species)
 
+taxa_acerto <- c()
+for(k in 1:10){
+  modelo <- knn(train = treino_padronizado, test = teste_padronizado, cl = classe_treino, k = k)
+  taxa_acerto[k] <- mean(modelo == teste$species)
+}
+df <- data.frame(k = 1:10, taxa_acerto)
+
+ggplot(data = df, aes(x = k, y = taxa_acerto))+
+  geom_line()
+  
