741fe58791dfbffdaf633f08a4d5fa7255828db5
ViniciusLimaCarvalho <viniciuscarvalho@ufu.br>
2024-08-28 17:37:04 -0300

    teste

--- Aulas/Aula7.R
+++ Aulas/Aula7.R
@@ -12,3 +12,8 @@ indices_treino <- sample(1:nrow(pinguins), size = n, replace = FALSE)
 
 treino <- pinguins[indices_treino,]
 teste <- pinguins[-indices_treino,]
+treino_padronizado <- scale(treino[,-1])
+teste_padronizado <- scale(teste[,-1])
+
+classe_treino <- treino$species
+classe_teste <- teste$species
